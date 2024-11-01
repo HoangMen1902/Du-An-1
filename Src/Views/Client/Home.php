56b0d3e9a287ffd820e77023a9e17bee4999173c
chihao <chihaothcsduong@gmail.com>
2024-11-01 20:35:18 +0700

    [html][client] thêm thành phần sản phẩm nổi bật cho trang chủ

--- Src/Views/Client/Home.php
+++ Src/Views/Client/Home.php
@@ -1,10 +1,9 @@
-
-
-<?php $this->layout('Client/Components/Layout');?>
+<?php $this->layout('Client/Components/Layout'); ?>
 
 
 <?php $this->start('main_content') ?>
 <!-- Insert nội dung vào đây -->
 
 
+<?php $this->Insert("/Client/Home/popular") ?>
 <?php $this->stop() ?>
---
+++ Src/Views/Client/Home/popular.php
@@ -0,0 +1,56 @@
+<?php $this->layout('Client/Components/Layout'); ?>
+
+
+<?php $this->start('main_content') ?>
+<!-- Insert nội dung vào đây -->
+<div class="container">
+    <div class="row">
+        <div class="popular-top  d-flex justify-content-between align-items-center">
+            <h3 class="popular-top__title border-bottom-custom">
+                Phổ biến nhất
+            </h3>
+            <div class="popular-top__button">
+                <button class="btn custom-btn-small">Xem thêm ></button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Hàng chứa các sản phẩm -->
+    <div class="row mt-4 ">
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="product-card">
+                <img src="https://www.phongcachxanh.vn/cdn/shop/files/chu-t-khong-day-sieu-nh-pulsar-xlite-v4-quiccs-edition-limited-1500c-di-kem-dongle-8k-polling-rate-42018806956277.jpg?v=1729690878&width=300" alt="Sản phẩm 1" class="img-fluid">
+                <h5 class="product-title">Chuột Không Dây Pulsar Xlite V4</h5>
+                <p class="product-price">1.000.000đ</p>
+                <button class="btn custom-btn add-to-cart-btn">Thêm vào giỏ</button>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="product-card">
+                <img src="https://www.phongcachxanh.vn/cdn/shop/files/pre-order-lot-chu-t-kinh-c-ng-l-c-tekkusai-the-beast-limited-42087967064309.jpg?v=1730188758&width=300" alt="Sản phẩm 2" class="img-fluid">
+                <h5 class="product-title">Chuột Tekkusai The Beast</h5>
+                <p class="product-price">1.000.000đ</p>
+                <button class="btn custom-btn add-to-cart-btn">Thêm vào giỏ</button>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="product-card">
+                <img src="https://www.phongcachxanh.vn/cdn/shop/files/pre-order-lot-chu-t-kinh-c-ng-l-c-gamesense-sonar-asuka-kimono-limited-edition-42050609938677.jpg?v=1729929750&width=300" alt="Sản phẩm 3" class="img-fluid">
+                <h5 class="product-title">Chuột Gamesense Sonar Asuka Kimono</h5>
+                <p class="product-price">1.000.000đ</p>
+                <button class="btn custom-btn add-to-cart-btn">Thêm vào giỏ</button>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="product-card">
+                <img src="https://www.phongcachxanh.vn/cdn/shop/files/pre-order-ban-phim-he-pulsar-pcmk-2-he-tkl-ansi-h-tr-rapid-trigger-42002910249205.jpg?v=1729624098&width=300" alt="Sản phẩm 4" class="img-fluid">
+                <h5 class="product-title">Bàn Phím Hê Pulsar PCM-K2</h5>
+                <p class="product-price">1.000.000đ</p>
+                <button class="btn custom-btn add-to-cart-btn">Thêm vào giỏ</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php $this->stop() ?>
