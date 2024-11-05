76c80cdab5d7c3931fb4240f5ecc858529d2089c
7CO <hoangmen9918@gmail.com>
2024-11-06 03:42:44 +0700

    [Migration] Tạo thêm một số Migrations

---
+++ database/migrations/20241105194333_options_table.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class OptionsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $table = $this->table('options');
+        $table->addColumn('name', 'string', ['limit' => 100]);
+        $table->addColumn('status', 'integer', ['default' => 1, 'null' => false]);
+        $table->addColumn('created_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP']);
+        $table->addColumn('updated_at', 'timestamp', ['null' => true]);
+        
+    }
+}
