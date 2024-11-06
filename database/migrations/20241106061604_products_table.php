aa025d609ec5872e66f0c085a3eed4b32edce828
NguyenHoaiBaoNe <nguyenhoaibao12c4@gmail.com>
2024-11-06 13:47:06 +0700

    [Migration] Products, Categories, Category_Values, Product_category

---
+++ database/migrations/20241106061604_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class ProductsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $table = $this->table('products');
+        $table->addColumn('name', 'string', ['limit' => 512])
+              ->addColumn('description', 'text', ['null' => true])
+              ->addColumn('total_quantity', 'integer', ['default' => 0])
+              ->addColumn('brand', 'string', ['limit' => 255, 'null' => true])
+              ->addColumn('status', 'integer', ['default' => 1]) 
+              ->addColumn('discount', 'decimal', ['precision' => 5, 'scale' => 2, 'default' => 0.00])
+              ->addColumn('thumbnail', 'string', ['limit' => 255, 'null' => true])
+              ->addColumn('created_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+              ->addColumn('updated_at', 'timestamp', [
+                  'default' => 'CURRENT_TIMESTAMP',
+                  'update' => 'CURRENT_TIMESTAMP'
+              ])
+              ->create();
+    }
+}
