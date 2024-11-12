8fd12a065a3bf4eeefa735bba41f372395f6197b
NguyenHoaiBaoNe <nguyenhoaibao12c4@gmail.com>
2024-11-12 17:16:16 +0700

    16. [System] - Client - Đổ dữ liệu lên trang sản phẩm

--- Src/Controllers/Client/ProductListController.php
+++ Src/Controllers/Client/ProductListController.php
@@ -2,10 +2,17 @@
     namespace Src\Controllers\Client;
 
     use Src\Controllers\BaseController;
+    use Src\Models\Client\ProductModel;
 
     class ProductListController extends BaseController {
         public function show() {
-            echo $this->view->render('Client/Pages/Product/List', ['Name' => 'Tien']);
+
+            $productModel = new ProductModel();
+            $productData = $productModel->getAllProductWithSkus();
+
+            echo $this->view->render('Client/Pages/Product/List',
+             ['productData' => $productData]
+            );
         }
     }
 ?>
--- Src/Models/Client/ProductModel.php
+++ Src/Models/Client/ProductModel.php
@@ -1,5 +1,5 @@
 <?php
-    namespace Src\Models\Admin;
+    namespace Src\Models\Client;
 
     use Src\Models\BaseModel;
 
@@ -13,6 +13,22 @@
             $id = (int) $id;
             return $this->getOne($id);
         }
+
+        public function getAllProductWithSkus() {
+            $sql = "SELECT products.*, product_skus.price AS product_price,  product_skus.images AS images
+                    FROM products
+                    JOIN product_skus ON product_skus.product_id = products.id";
+    
+            $conn = $this->_conn->MySQLi();
+            $result = $conn->query($sql);
+            if ($result) {
+                $products = $result->fetch_all(MYSQLI_ASSOC);
+                return $products;
+            } else {
+                return [];
+            }
+        }
+        
     }
 
 
