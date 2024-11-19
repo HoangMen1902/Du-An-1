5d80bddf062d41927def31b800ec9b28d7aa06ed
NhutTien <dangnhuttien1004@gmail.com>
2024-11-19 10:32:27 +0700

    [45][System][Client] Đỗ dữ liệu trang giỏ hàng

--- Src/Views/Client/Pages/Cart.php
+++ Src/Views/Client/Pages/Cart.php
@@ -23,177 +23,53 @@
                 </div>
                 <table class="cart__table">
 
-                    <tr class="cart__product">
-                        <td style="width: 15%;">
-                            <img class="cart__product-image" src="https://www.phongcachxanh.vn/cdn/shop/files/pre-order-lot-chu-t-kinh-c-ng-l-c-tekkusai-the-beast-limited-42087967064309.jpg?v=1730188758&width=300" alt="Chuột không dây siêu nhẹ Lamzu Atlantis Mini Pro - Hỗ trợ 4KHz">
-                        </td>
+                    <?php foreach ($Data as $cart): ?>
+                        <tr class="cart__product">
+                            <td style="width: 15%;">
+                                <img class="cart__product-image" src="<?= $_ENV['APP_URL'] ?>/public/Uploads/Products/<?= $cart['product_images'] ?>" alt="<?= htmlspecialchars($cart['product_sku']) ?>">
+                            </td>
 
-                        <td style="width: 55%; ">
-                            <div class="cart__product-details">
-                                <h2 class="cart__product-name">Lót chuột Lethal Gaming Gear Jupiter PRO </h2>
-                                <p class="cart__product-price">1.240.000₫
-                                </p>
-                                <!-- <p class="cart__product-description clamp-text"><?= $cart['data']['description'] ?></p> -->
-                                <div class="cart__product-description clamp-text">
-                                    <ul class="cart__product-description-fix">
-                                        <li>Thuộc phân loại Control - Slow, chậm nhất trong các dòng PRO của Lethal Gaming.</li>
-                                    </ul>
-
+                            <td style="width: 55%; ">
+                                <div class="cart__product-details">
+                                    <h2 class="cart__product-name"><?= htmlspecialchars($cart['product_name'] . ''. $cart['product_sku']) ?></h2>
+                                    <p class="cart__product-price"><?= number_format($cart['product_price'], 0, ',', '.') ?>₫</p>
+                                    <div class="cart__product-description ">
+                                        <ul class="cart__product-description-fix">
+                                            <li class=" text-limit" ><?= htmlspecialchars($cart['product_description']) ?>.</li>
+                                        </ul>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td style="width: 15%;">
-                            <form action="/update-cart" method="post" class="quantity-form">
-                                <input type="hidden" name="method" value="POST">
-                                <input type="hidden" name="id" value="">
-                                <input class="cart__product-quantity" type="text" name="quantity" value="1" onchange="this.form.submit()">
-                                <input type="hidden" name="update-cart-item">
-                            </form>
-                            <form action="/delete-cart-item" method="post" class="delete-form">
-                                <input type="hidden" name="id" value="">
-                                <input type="hidden" name="method" value="POST">
-                                <button name="delete-cart-item" onclick="return confirm('Bạn chắc chắn muốn xóa sản phẩm này ra khỏi giỏ hàng chứ?')">
-                                    <svg class="cart__content__fix1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                    </svg>
-                                </button>
+                            </td>
 
-                            </form>
+                            <td style="width: 15%;">
+                                <form action="/update-cart" method="post" class="quantity-form">
+                                    <input type="hidden" name="method" value="POST">
+                                    <input type="hidden" name="id" value="<?= htmlspecialchars($cart['cart_id']) ?>">
+                                    <input class="cart__product-quantity" type="text" name="quantity" value="<?= htmlspecialchars($cart['quantity']) ?>" onchange="this.form.submit()">
+                                    <input type="hidden" name="update-cart-item">
+                                </form>
 
-                        </td>
-                        <td class="price" style="width: 15%;">
-                            <p class="cart__product-total">1.240.000₫</p>
-                        </td>
-                    </tr>
-                    <tr class="cart__product">
-                        <td style="width: 15%;">
-                            <img class="cart__product-image" src="https://www.phongcachxanh.vn/cdn/shop/files/pre-order-lot-chu-t-kinh-c-ng-l-c-tekkusai-the-beast-limited-42087967064309.jpg?v=1730188758&width=300" alt="Chuột không dây siêu nhẹ Lamzu Atlantis Mini Pro - Hỗ trợ 4KHz">
-                        </td>
+                                <form action="/delete-cart-item" method="post" class="delete-form">
+                                    <input type="hidden" name="id" value="<?= htmlspecialchars($cart['cart_id']) ?>">
+                                    <input type="hidden" name="method" value="POST">
+                                    <button name="delete-cart-item" onclick="return confirm('Bạn chắc chắn muốn xóa sản phẩm này ra khỏi giỏ hàng chứ?')">
+                                        <svg class="cart__content__fix1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                        </svg>
+                                    </button>
+                                </form>
+                            </td>
 
-                        <td style="width: 55%; ">
-                            <div class="cart__product-details">
-                                <h2 class="cart__product-name">Lót chuột Lethal Gaming Gear Jupiter PRO </h2>
-                                <p class="cart__product-price">1.240.000₫
-                                </p>
-                                <!-- <p class="cart__product-description clamp-text"><?= $cart['data']['description'] ?></p> -->
-                                <div class="cart__product-description clamp-text">
-                                    <ul class="cart__product-description-fix">
-                                        <li>Thuộc phân loại Control - Slow, chậm nhất trong các dòng PRO của Lethal Gaming.</li>
-                                    </ul>
+                            <td class="price" style="width: 15%;">
+                                <p class="cart__product-total"><?= number_format($cart['total_price'], 0, ',', '.') ?>₫</p>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
 
-                                </div>
-                            </div>
-                        </td>
-                        <td style="width: 15%;">
-                            <form action="/update-cart" method="post" class="quantity-form">
-                                <input type="hidden" name="method" value="POST">
-                                <input type="hidden" name="id" value="">
-                                <input class="cart__product-quantity" type="text" name="quantity" value="1" onchange="this.form.submit()">
-                                <input type="hidden" name="update-cart-item">
-                            </form>
-                            <form action="/delete-cart-item" method="post" class="delete-form">
-                                <input type="hidden" name="id" value="">
-                                <input type="hidden" name="method" value="POST">
-                                <button name="delete-cart-item" onclick="return confirm('Bạn chắc chắn muốn xóa sản phẩm này ra khỏi giỏ hàng chứ?')">
-                                    <svg class="cart__content__fix1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                    </svg>
-                                </button>
 
-                            </form>
-
-                        </td>
-                        <td class="price" style="width: 15%;">
-                            <p class="cart__product-total">1.240.000₫</p>
-                        </td>
-                    </tr>
-                    <tr class="cart__product">
-                        <td style="width: 15%;">
-                            <img class="cart__product-image" src="https://www.phongcachxanh.vn/cdn/shop/files/pre-order-lot-chu-t-kinh-c-ng-l-c-tekkusai-the-beast-limited-42087967064309.jpg?v=1730188758&width=300" alt="Chuột không dây siêu nhẹ Lamzu Atlantis Mini Pro - Hỗ trợ 4KHz">
-                        </td>
-
-                        <td style="width: 55%; ">
-                            <div class="cart__product-details">
-                                <h2 class="cart__product-name">Lót chuột Lethal Gaming Gear Jupiter PRO </h2>
-                                <p class="cart__product-price">1.240.000₫
-                                </p>
-                                <!-- <p class="cart__product-description clamp-text"><?= $cart['data']['description'] ?></p> -->
-                                <div class="cart__product-description clamp-text">
-                                    <ul class="cart__product-description-fix">
-                                        <li>Thuộc phân loại Control - Slow, chậm nhất trong các dòng PRO của Lethal Gaming.</li>
-                                    </ul>
-
-                                </div>
-                            </div>
-                        </td>
-                        <td style="width: 15%;">
-                            <form action="/update-cart" method="post" class="quantity-form">
-                                <input type="hidden" name="method" value="POST">
-                                <input type="hidden" name="id" value="">
-                                <input class="cart__product-quantity" type="text" name="quantity" value="1" onchange="this.form.submit()">
-                                <input type="hidden" name="update-cart-item">
-                            </form>
-                            <form action="/delete-cart-item" method="post" class="delete-form">
-                                <input type="hidden" name="id" value="">
-                                <input type="hidden" name="method" value="POST">
-                                <button name="delete-cart-item" onclick="return confirm('Bạn chắc chắn muốn xóa sản phẩm này ra khỏi giỏ hàng chứ?')">
-                                    <svg class="cart__content__fix1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                    </svg>
-                                </button>
-
-                            </form>
-
-                        </td>
-                        <td class="price" style="width: 15%;">
-                            <p class="cart__product-total">1.240.000₫</p>
-                        </td>
-                    </tr>
-                    <tr class="cart__product">
-                        <td style="width: 15%;">
-                            <img class="cart__product-image" src="https://www.phongcachxanh.vn/cdn/shop/files/pre-order-lot-chu-t-kinh-c-ng-l-c-tekkusai-the-beast-limited-42087967064309.jpg?v=1730188758&width=300" alt="Chuột không dây siêu nhẹ Lamzu Atlantis Mini Pro - Hỗ trợ 4KHz">
-                        </td>
-
-                        <td style="width: 55%; ">
-                            <div class="cart__product-details">
-                                <h2 class="cart__product-name">Lót chpiter PRO </h2>
-                                <p class="cart__product-price">1.240.0qweqwe q00₫
-                                </p>
-                                <!-- <p class="cart__product-description clamp-text"><?= $cart['data']['description'] ?></p> -->
-                                <div class="cart__product-description clamp-text">
-                                    <ul class="cart__product-description-fix">
-                                        <li>Thuộc phác dòng PRO của Lethal Gaming.</li>
-                                    </ul>
-
-                                </div>
-                            </div>
-                        </td>
-                        <td style="width: 15%;">
-                            <form action="/update-cart" method="post" class="quantity-form">
-                                <input type="hidden" name="method" value="POST">
-                                <input type="hidden" name="id" value="">
-                                <input class="cart__product-quantity" type="text" name="quantity" value="1" onchange="this.form.submit()">
-                                <input type="hidden" name="update-cart-item">
-                            </form>
-                            <form action="/delete-cart-item" method="post" class="delete-form">
-                                <input type="hidden" name="id" value="">
-                                <input type="hidden" name="method" value="POST">
-                                <button name="delete-cart-item" onclick="return confirm('Bạn chắc chắn muốn xóa sản phẩm này ra khỏi giỏ hàng chứ?')">
-                                    <svg class="cart__content__fix1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                    </svg>
-                                </button>
-
-                            </form>
-
-                        </td>
-                        <td class="price" style="width: 15%;">
-                            <p class="cart__product-total">1.240.000₫</p>
-                        </td>
-                    </tr>
                 </table>
             </div>
-            
+
             <div class="cart__summary">
                 <div class="cart__summary-details">
                     <div class="cart__summary-item">
